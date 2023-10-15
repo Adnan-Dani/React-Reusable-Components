dcf36bf58710508e08a196287550a72a900d05d9
Muhammad Adnan <61709395+Adnan-Dani@users.noreply.github.com>
2023-10-15 22:56:49 +0500

    New Component -> ExpandableText

---
+++ ExpandableText/ExapndableText.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+interface Props {
+    children: string;
+    maxChars?: number;
+}
+const ExapndableText = ({ children, maxChars = 100 }: Props) => {
+    const [isExpanded, setIsExpanded] = useState(false)
+    if (children.length <= maxChars) return <p>{children}</p>;
+
+    const text = isExpanded ? children : children.substring(0, maxChars);
+    return <p>{text}... <button onClick={() => setIsExpanded(!isExpanded)}> {isExpanded ? "Less" : "More"} </button> </p>
+
+}
+
+export default ExapndableText
